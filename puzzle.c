37aaacdf517766484cb598a79d5fa7921952512c
lmolina32 <lmolina3@nd.edu>
2024-09-19 15:12:02 -0400

    hard coded sudoku puzzle

--- puzzle.c
+++ puzzle.c
@@ -20,7 +20,7 @@ int ** createPuzzle(){
 
         puzzle[i] = (int*)malloc(sizeof(int)*9);
 
-        for (j = 0; i < 9; j++){
+        for (j = 0; j < 9; j++){
 
             puzzle[i][j] = array[i][j];
         }
@@ -32,12 +32,19 @@ void printPuzzle(int ** puzzle){
 
     int i, j;
 
-    printf(" ");
+    printf("--------------------------------\n");
     for (i = 0; i < 9; i++ ){
+        printf("|");
         //print each row 
         for (j = 0; j < 9; j++){
             printf(" %d ", puzzle[i][j]);
+            if ((j+1) % 3 == 0){
+                printf("|");
+            }
         }
         printf("\n");
+        if (( i + 1) % 3 == 0){
+            printf("--------------------------------\n");
+        }
     }
 }
