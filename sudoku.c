c93a145348f739b96c6063608703382c242b7754
lmolina32 <lmolina3@nd.edu>
2024-09-19 21:40:37 -0400

    created struct to store info for every value in puzzle

--- sudoku.c
+++ sudoku.c
@@ -1,10 +1,13 @@
 #include "sudoku.h"
 int main(){
     int ** puzzle;
+    Square *** sudoku;
 
     puzzle = createPuzzle();
 
     printPuzzle(puzzle);
     
+    sudoku = setUpPuzzle(puzzle);
+
     return 0;
 }
--- sudoku.h
+++ sudoku.h
@@ -1,5 +1,22 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+char POSSIBLE = 0x1FF;
+
+typedef struct Box{
+    struct Box * next;
+} Box;
+typedef struct Square{
+    int number;
+    /* 000000000 
+       987654321*/
+    char code;
+    Box * box;
+    int row;
+    int column;
+} Square;
+
 int ** createPuzzle();
 void printPuzzle(int ** puzzle);
+Square *** setUpPuzzle(int ** puzzle);
+
